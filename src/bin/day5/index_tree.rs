c1bc7eba60db47205675cbe3d73b09d2d2eaf96e
Alex Mit <klepoz.672@mail.ru>
2024-09-17 17:45:25 +0300

    day5: index_tree progress

--- src/bin/day5/index_tree.rs
+++ src/bin/day5/index_tree.rs
@@ -15,20 +15,31 @@ impl IntervalTree {
     pub fn insert(&mut self, data: NodeData) -> bool {
         if let None = self.root {
             self.root = Some(self.alloc_node(data, 0));
+        } else if !self.insert_at(self.root.unwrap(), data) {
+            return false;
         }
-        // ...
         self.count += 1;
         true
     }
 
-    // fn isert_at(&mut self, at_node: NodeHandle, data: NodeData) -> bool {
-    //     if data == self.nodes[&at_node].data {
-    //         return false;
-    //     }
-    //     if data.self_interval.low < self.nodes[&at_node].data.self_interval.low {
-    //         if self.nodes[&at_node].left == 0 {}
-    //     }
-    // }
+    fn insert_at(&mut self, at_node: NodeHandle, data: NodeData) -> bool {
+        if data == self.nodes[&at_node].data {
+            return false;
+        }
+        if data.self_interval.low < self.nodes[&at_node].data.self_interval.low {
+            if self.nodes[&at_node].left.is_none() {
+                self.nodes.get_mut(&at_node).unwrap().left = Some(self.alloc_node(data, at_node));
+                true
+            } else {
+                self.insert_at(self.nodes[&at_node].left.unwrap(), data)
+            }
+        } else if self.nodes[&at_node].right.is_none() {
+            self.nodes.get_mut(&at_node).unwrap().right = Some(self.alloc_node(data, at_node));
+            true
+        } else {
+            self.insert_at(self.nodes[&at_node].right.unwrap(), data)
+        }
+    }
 
     fn alloc_node(&mut self, data: NodeData, parent: NodeHandle) -> NodeHandle {
         self.nodes
@@ -47,9 +58,9 @@ struct NodeData {
 struct Node {
     data: NodeData,
     max_value: usize,
-    left: NodeHandle,
-    right: NodeHandle,
-    parent: NodeHandle,
+    left: Option<NodeHandle>,
+    right: Option<NodeHandle>,
+    parent: Option<NodeHandle>,
 }
 
 impl Node {
@@ -57,18 +68,18 @@ impl Node {
         Self {
             max_value: data.self_interval.high,
             data,
-            left: 0,
-            right: 0,
-            parent: 0,
+            left: None,
+            right: None,
+            parent: None,
         }
     }
     fn new_with_parent(data: NodeData, parent: NodeHandle) -> Self {
         Self {
             max_value: data.self_interval.high,
             data,
-            left: 0,
-            right: 0,
-            parent,
+            left: None,
+            right: None,
+            parent: Some(parent),
         }
     }
 }
